d4429dfb100da49944fb6c79b591dfbc319bfaa0
Spencer Harston <spencer314@gmail.com>
2019-11-11 22:28:34 -0700

    Start work on time elapsed since story creation

---
+++ hn/README.md
@@ -0,0 +1,4 @@
+# Hacker News Clone
+A simple application utilizing the [Hacker News API](https://github.com/HackerNews/API). Yeah, kind of looks like the main [Hacker News site](https://news.ycombinator.com), but that will change as progress is made.  
+
+Goal is practice using JavaScript and APIs. 
--- hn/script.js
+++ hn/script.js
@@ -60,9 +60,11 @@ function createStoryCard(story){
     let headlineDiv = document.createElement('div');
     headlineDiv.setAttribute('class', 'headline');
 
-    let storyTitle = document.createElement('span');
+    let storyTitle = document.createElement('a');
     storyTitle.setAttribute('class', 'title')
-    storyTitle.textContent = story.title;
+    storyTitle.setAttribute('href', story.url);
+    storyTitle.setAttribute('targe', '_blank');
+    storyTitle.innerHTML = story.title;
 
     let domain = document.createElement('span');
     domain.setAttribute('class', 'domain');
@@ -80,11 +82,6 @@ function createStoryCard(story){
     let linkDiv = document.createElement('div');
     linkDiv.setAttribute('class', 'links');
 
-    let articleLink = document.createElement('a');
-    articleLink.setAttribute('href', story.url);
-    articleLink.setAttribute('target', '_blank');
-    articleLink.innerHTML = 'Article Link';
-
     let hnLink = document.createElement('a');
     hnLink.setAttribute('href', `https://news.ycombinator.com/item?id=${story.id}`);
     hnLink.setAttribute('target', '_blank');
@@ -95,6 +92,10 @@ function createStoryCard(story){
     } else{
         hnLink.innerHTML = `0 Comments`;
     }
+
+    let score = document.createElement('span');
+    score.setAttribute('class', 'score');
+    score.textContent = `${story.score} points by ${story.by} at ${getFriendlyTime(story.time)}`;
 
     //add the story element
     container.appendChild(storyCard);
@@ -108,7 +109,7 @@ function createStoryCard(story){
     headlineDiv.appendChild(domain);
     
     //add links element
-    linkDiv.appendChild(articleLink);
+    linkDiv.appendChild(score);
     linkDiv.appendChild(hnLink);
 
 }
@@ -137,4 +138,33 @@ function getDomain(url){
     domain = url.split('/')[0];
 
     return domain;
+}
+
+function getFriendlyTime(timestamp){
+    var time = new Date(timestamp * 1000);
+
+    getElapsedTime(time);
+     
+    return time;
+}
+
+function getElapsedTime(oldDate){
+
+    var now = new Date();
+
+    var timeDiff = now.getTime() - oldDate.getTime();
+
+    console.log(timeDiff);
+
+    var seconds = Math.floor(timeDiff / 1000); 
+    var minutes = Math.floor(seconds / 60); 
+    var hours = Math.floor(minutes / 60);
+    var days = Math.floor(hours / 24);
+
+    console.log(`Old: ${oldDate}`);
+    console.log(`New: ${now}`);
+    console.log(`Days: ${days}`);
+    console.log(`Hours: ${hours}`);
+    console.log(`Minutes: ${minutes}`);
+    console.log(`Seconds: ${seconds}`);
 }
